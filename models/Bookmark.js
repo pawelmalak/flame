ee9aefa4fa11799cff399e43c38e5447677d7ee7
Paweł Malak <pawel999@icloud.com>
2021-11-08 23:39:42 +0100

    Modified models to support resource visibility. Added migration

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,25 +1,34 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../db');
 
-const Bookmark = sequelize.define('Bookmark', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
+const Bookmark = sequelize.define(
+  'Bookmark',
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    icon: {
+      type: DataTypes.STRING,
+      defaultValue: '',
+    },
+    isPublic: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 0,
+    },
   },
-  url: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  categoryId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  icon: {
-    type: DataTypes.STRING,
-    defaultValue: ''
+  {
+    tableName: 'bookmarks',
   }
-}, {
-  tableName: 'bookmarks'
-});
+);
 
 module.exports = Bookmark;
