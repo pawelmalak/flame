d13b890e16ae0d128793f1329416ba034f04fc47
Paweł Malak <pawel999@icloud.com>
2021-11-19 14:06:38 +0100

    Fixed bug with value parsing if custom icon was used

--- client/src/utility/applyAuth.ts
+++ client/src/utility/applyAuth.ts
@@ -1,4 +1,4 @@
 export const applyAuth = () => {
   const token = localStorage.getItem('token') || '';
-  return { Authorization_Flame: `Bearer ${token}` };
+  return { 'Authorization-Flame': `Bearer ${token}` };
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const authHeader = req.header('Authorization_Flame');
+  const authHeader = req.header('Authorization-Flame');
   let token;
   let tokenIsValid = false;
 
