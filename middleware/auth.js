e13f6f261200811837c213179eb90cd34adb087f
Paweł Malak <pawel999@icloud.com>
2021-11-19 12:41:32 +0100

    Changed auth header

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const authHeader = req.header('Authorization');
+  const authHeader = req.header('Authorization_Flame');
   let token;
   let tokenIsValid = false;
 
